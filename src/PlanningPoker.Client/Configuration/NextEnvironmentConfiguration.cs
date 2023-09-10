54c26869c52eec5b45e6ea40302289f74f7145ff
Andreas R. L. Christiansen <AndreasRLC@gmail.com>
2023-09-10 14:21:50 +0200

    Added Blazor configuration, and feature to promote Nightly when in production, and production when in Nightly.

---
+++ src/PlanningPoker.Client/Configuration/NextEnvironmentConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace PlanningPoker.Client.Configuration
+{
+    public class NextEnvironmentConfiguration
+    {
+        public const string Path = "UI:Features:NextEnvironment";
+
+        public string RichText { get; set; }
+    }
+}
