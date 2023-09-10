601442fbf61b543b10a250a8ce6881fc596f94d6
Andreas R. L. Christiansen <AndreasRLC@gmail.com>
2023-09-10 18:22:56 +0200

    Fixed launch settings for environments.

--- src/PlanningPoker.Server/Program.cs
+++ src/PlanningPoker.Server/Program.cs
@@ -19,6 +19,18 @@ namespace PlanningPoker.Server
                     .AddCommandLine(args)
                     .Build())
                 .UseStartup<Startup>()
+
+                // Leaving commented code in the codebase is not a good idea,
+                // but I think this warrants an exception.
+                // Blazor does not allow local debugging where you set the ASPNETCORE_ENVIRONMENT to anything
+                // other than Development: https://github.com/dotnet/aspnetcore/issues/43110#issuecomment-1206573745.
+                // In order to test environment-specific configuration,
+                // the below line has to be added.
+                // Note, this line should *not* be added in production code,
+                // as it enables locally run code to get static assets from the build output.
+                // We don't want that behavior on an environment running on published files.
+                //.UseStaticWebAssets() // <-- This is the line to comment in to test environments.
+                // End comment block.
                 .Build();
         }
     }
